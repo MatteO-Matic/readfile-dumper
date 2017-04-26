17560f1530851e42141dd73ac5ddba302f9e3487
Matte <matteomatic@outlook.com>
2017-04-26 18:28:23 +0200

    loop the read and write, easier to test

--- readfile_test/main.cpp
+++ readfile_test/main.cpp
@@ -6,29 +6,36 @@ using namespace std;
 
 int main()
 {
-  ofstream file;
-  file.open("a.txt");
-  for(int i = 0; i < 10;++i)
+  while(1)
   {
-    file << i << " :Hello world\n";
-  }
+    cout << "Click me\n";
+    string input;
+    cin >> input;
 
-  file.close();
-
-
-  ifstream rfile("a.txt");
-  string line;
-  if(rfile.is_open())
-  {
-    while(getline(rfile, line))
+    ofstream file;
+    file.open("a.txt");
+    for(int i = 0; i < 10;++i)
     {
-      cout << line << '\n';
+      file << i << " :Hello world\n";
     }
-    rfile.close();
-  }
-  else
-  {
-    cout << "Unable to open file";
+
+    file.close();
+
+
+    ifstream rfile("a.txt");
+    string line;
+    if(rfile.is_open())
+    {
+      while(getline(rfile, line))
+      {
+        cout << line << '\n';
+      }
+      rfile.close();
+    }
+    else
+    {
+      cout << "Unable to open file";
+    }
   }
   return 0;
 }
