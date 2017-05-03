56b5d3f6b6a8ba0daea4acca05437b71df184a0e
Matte <matteomatic@outlook.com>
2017-05-03 16:30:48 +0200

    more tests.

--- readfile_test/main.cpp
+++ readfile_test/main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -36,6 +37,59 @@ int main()
     {
       cout << "Unable to open file";
     }
+
+
+
+    rfile.open("b.txt");
+    if(rfile.is_open())
+    {
+      while(getline(rfile, line))
+      {
+
+      }
+      rfile.close();
+    }
+    else
+    {
+      cout << "Unable to open file";
+    }
+    //read large from folder
+    rfile.open("files/large.txt");
+    if(rfile.is_open())
+    {
+      string dummy;
+      int counter = 0;
+      while(getline(rfile, line))
+      {
+        dummy = line;
+        counter++;
+        if(counter % 500 == 0 )
+        {
+          cout << dummy << "\n";
+        }
+      }
+      rfile.close();
+    }
+    else
+    {
+      cout << "Unable to open file";
+    }
+
+
+    //Read zip
+    ifstream flarge("somezip.zip", ios::ate | ios::binary);
+    if(flarge.is_open())
+    {
+      cout << "large file is open\n";
+      ifstream::pos_type pos = flarge.tellg();
+      vector<char> buffer(pos);
+      flarge.seekg(0, ios::beg);
+      flarge.read(&buffer[0], pos);
+    }
+
+
+
+    flarge.close();
   }
   return 0;
 }
