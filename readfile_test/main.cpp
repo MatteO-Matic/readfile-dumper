f05af181707d43d621d822686ad1985e24be9454
Matte <matteomatic@outlook.com>
2017-05-04 11:03:56 +0200

    made tests into functions, added multiple reads on same file

--- readfile_test/main.cpp
+++ readfile_test/main.cpp
@@ -5,91 +5,108 @@
 
 using namespace std;
 
+
+void read1()
+{
+
+  ifstream rfile("a.txt");
+  string line;
+  if(rfile.is_open())
+  {
+    while(getline(rfile, line))
+    {
+      cout << line << '\n';
+    }
+    rfile.close();
+  }
+  else
+  {
+    cout << "Unable to open file";
+  }
+}
+
+void read2()
+{
+  string line;
+  ifstream rfile("b.txt");
+  if(rfile.is_open())
+  {
+    while(getline(rfile, line))
+    {
+
+    }
+    rfile.close();
+  }
+  else
+  {
+    cout << "Unable to open file";
+  }
+}
+
+void read3()
+{
+  //read large from folder
+  string line;
+  ifstream rfile("files/large.txt");
+  if(rfile.is_open())
+  {
+    string dummy;
+    int counter = 0;
+    while(getline(rfile, line))
+    {
+      dummy = line;
+      counter++;
+      if(counter % 500 == 0 )
+      {
+        cout << dummy << "\n";
+      }
+    }
+    rfile.close();
+  }
+  else
+  {
+    cout << "Unable to open file";
+  }
+}
+
+//Read a full chunck into buffer
+void read4()
+{
+  //Read zip
+  ifstream flarge("somezip.zip", ios::ate | ios::binary);
+  if(flarge.is_open())
+  {
+    cout << "large file is open\n";
+    ifstream::pos_type pos = flarge.tellg();
+    vector<char> buffer(pos);
+    flarge.seekg(0, ios::beg);
+    flarge.read(&buffer[0], pos);
+  }
+  flarge.close();
+}
+
 int main()
 {
+  ofstream file;
+  file.open("a.txt");
+  for(int i = 0; i < 10;++i)
+  {
+    file << i << " :Hello world\n";
+  }
+
+  file.close();
   while(1)
   {
     cout << "Click me\n";
     string input;
     cin >> input;
 
-    ofstream file;
-    file.open("a.txt");
-    for(int i = 0; i < 10;++i)
-    {
-      file << i << " :Hello world\n";
-    }
+    read1();
+    read2();
+    read3();
+    read4();
+    read1(); //multiple reads on same file
 
-    file.close();
-
-
-    ifstream rfile("a.txt");
-    string line;
-    if(rfile.is_open())
-    {
-      while(getline(rfile, line))
-      {
-        cout << line << '\n';
-      }
-      rfile.close();
-    }
-    else
-    {
-      cout << "Unable to open file";
-    }
-
-
-
-    rfile.open("b.txt");
-    if(rfile.is_open())
-    {
-      while(getline(rfile, line))
-      {
-
-      }
-      rfile.close();
-    }
-    else
-    {
-      cout << "Unable to open file";
-    }
-    //read large from folder
-    rfile.open("files/large.txt");
-    if(rfile.is_open())
-    {
-      string dummy;
-      int counter = 0;
-      while(getline(rfile, line))
-      {
-        dummy = line;
-        counter++;
-        if(counter % 500 == 0 )
-        {
-          cout << dummy << "\n";
-        }
-      }
-      rfile.close();
-    }
-    else
-    {
-      cout << "Unable to open file";
-    }
-
-
-    //Read zip
-    ifstream flarge("somezip.zip", ios::ate | ios::binary);
-    if(flarge.is_open())
-    {
-      cout << "large file is open\n";
-      ifstream::pos_type pos = flarge.tellg();
-      vector<char> buffer(pos);
-      flarge.seekg(0, ios::beg);
-      flarge.read(&buffer[0], pos);
-    }
-
-
-
-    flarge.close();
+    return 0;
   }
-  return 0;
 }
